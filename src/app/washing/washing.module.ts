2192abfd85fcf026e3b0874d2d97aaeba2febcd7
Johnner Yelcde <johnnercastrojy@gmail.com>
2024-08-20 15:07:41 -0300

    Começando Escolha de Lavagem

--- src/app/washing/washing.module.ts
+++ src/app/washing/washing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReservationFormComponent } from './reservation-form/reservation-form.component';
+import { TypeOfWashingComponent } from './type-of-washing/type-of-washing.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
 @NgModule({
   declarations: [
-    ReservationFormComponent
+    ReservationFormComponent,
+    TypeOfWashingComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MaterialModule
+  ],
+  exports: [TypeOfWashingComponent]
 })
 export class WashingModule { }
