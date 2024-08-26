92a909bebf90432047de2b4b259c09a8dcb34e0e
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2024-08-25 22:46:17 -0300

    updating all styles

--- src/app/washing/type-of-washing/type-of-washing.component.ts
+++ src/app/washing/type-of-washing/type-of-washing.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-type-of-washing',
   templateUrl: './type-of-washing.component.html',
-  styleUrl: './type-of-washing.component.css'
+  styleUrl: './type-of-washing.component.css',
 })
-export class TypeOfWashingComponent {
-
-}
+export class TypeOfWashingComponent {}
